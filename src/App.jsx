1f693919c100244aee68e4f0af6d240a993f509d
Jocelyna <jocelynaeffendi@gmail.com>
2026-05-13 19:46:42 +0700

    trailer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Trailer from './components/trailer';
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Trailer/>
+    </>
   );
 }
 
--- src/components/trailer.jsx
+++ src/components/trailer.jsx
@@ -1,2 +1,12 @@
-export default function trailer(){
+export default function Trailer(){
+    return(
+        <section className="flex flex-col lg:flex-row items-center p-4">
+            <div className="bg-card flex flex-col rounded-3xl p-4">
+                <h1 className="text-3xl lg:text-5xl mb-4 font-days-one glow ">What is HISHOT?</h1>
+                <p className="font-chivo text-blue-info text-sm lg:text-base">
+                    HISHOT 2026 is an annual event organized by HIMTI BINUS University that focuses on Cloud Technology and Digital Infrastructure. With the theme “ORBIT: Optimizing Resource for Building Infrastructure and Technology”, HISHOT 2026 features a series of seminars, workshops, and study tours, offering both onsite and online experiences. This event highlights the importance of efficient resource management in today’s rapidly evolving digital era. HISHOT 2026 provides a platform to learn, collaborate, and grow in the field of digital infrastructure and emerging technologies.
+                </p>
+            </div>
+        </section>
+    );
 }
