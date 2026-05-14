961761c557caee4b4a468e63d777e3631bd5cb64
Jocelyna <jocelynaeffendi@gmail.com>
2026-05-14 14:16:25 +0700

    trailer

--- src/components/trailer.jsx
+++ src/components/trailer.jsx
@@ -1,19 +1,20 @@
 export default function Trailer(){
     return(
-        <section className="flex flex-col lg:flex-row items-center md:p-10 gap-0">
-            <div className="md:rounded-3xl md:p-4 sm:bg-card">
-                <h1 className="text-3xl md:text-5xl p-4 pr-12 pb-2 sm:mb-4 font-days-one glow w-3/4 bg-card z-10 inverted-radius">What is HISHOT?</h1>
-                <p className="font-chivo text-blue-info text-sm md:text-base bg-card p-4 -mt-2 border-2 border-[#F9FEFE] z-0">
-                    HISHOT 2026 is an annual event organized by HIMTI BINUS University that focuses on Cloud Technology and Digital Infrastructure. With the theme “<b>ORBIT: Optimizing Resource for Building Infrastructure and Technology</b>”, HISHOT 2026 features a series of seminars, workshops, and study tours, offering both onsite and online experiences. This event highlights the importance of efficient resource management in today’s rapidly evolving digital era. HISHOT 2026 provides a platform to learn, collaborate, and grow in the field of digital infrastructure and emerging technologies.
-                </p>
-            </div>
-            <div className="bg-card p-4 -mt-4 w-full flex justify-center flex-col border-b-2 border-[#F9FEFE]">
-                <div className="self-center bg-[#F8A819] w-fit border-[1.8px] border-[#F9FEFE] pt-2 pb-2 pr-4 pl-4 rounded-2xl -mb-6.5 z-10 shadow-[0_1.8px_1.8px_rgba(0,0,0,0.25)] opacity-90">
-                    <h2 className="font-days-one text-2xl text-white text-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.25)]">Trailer</h2>
+        <section className="relative">
+            <section className="flex flex-col md:flex-row md:p-22 gap-0 md:items-stretch">
+                <div className=" rounded-2xl sm:rounded-3xl md:p-4 md:bg-card md:flex-1 md:border-2 border-[#F9FEFE] md:pb-20 md:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+                    <h1 className="text-3xl lg:text-4xl p-4 pr-12 pb-2 font-days-one glow w-3/4 md:w-fit bg-card z-10 inverted-radius">What is HISHOT?</h1>
+                    <p className="font-chivo text-blue-info text-sm lg:text-base bg-card p-4 border-t-2 border-[#F9FEFE] z-0 md:border-0">
+                        HISHOT 2026 is an annual event organized by HIMTI BINUS University that focuses on Cloud Technology and Digital Infrastructure. With the theme “<b>ORBIT: Optimizing Resource for Building Infrastructure and Technology</b>”, HISHOT 2026 features a series of seminars, workshops, and study tours, offering both onsite and online experiences. This event highlights the importance of efficient resource management in today’s rapidly evolving digital era. HISHOT 2026 provides a platform to learn, collaborate, and grow in the field of digital infrastructure and emerging technologies.
+                    </p>
                 </div>
-                {/* Masih dalam revisi */}
-                <iframe src="" frameborder="0" className="w-full border-2 border-blue-950"></iframe>
-            </div>
+                <div className="bg-card p-4 md:p-6 lg:p-10 -mt-4 w-full flex justify-center flex-col border-b-2 border-[#F9FEFE] md:flex-1 sm:mt-0 md:relative md:top-10 md:rounded-4xl md:border-2 pb-8 max-h-150 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+                    <h2 className="font-days-one text-2xl lg:text-4xl text-white text-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.25)] self-center bg-[#F8A819]/90 w-fit border-[1.8px] border-[#F9FEFE] pt-4 pb-4 pr-4 pl-4 rounded-2xl md:rounded-3xl -mb-8 z-10 shadow-[0_1.8px_1.8px_rgba(0,0,0,0.25)]">Trailer</h2>
+                    {/* Masih dalam revisi */}
+                    <iframe src="" frameborder="0" className="w-full border-2 border-blue-950 bg-white h-full"></iframe>
+                </div>
+            </section>
+            <img src="/Mascot1.svg" alt="Mascot" />
         </section>
     );
 }
